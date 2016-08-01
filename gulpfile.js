e19aac277b5b43e6d5d54e8371f2eca8ed7ceb28
Roy A Vanegas <roy@thecodeeducators.com>
2016-08-01 06:37:07 -0400

    Implement the clean task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 /*jslint node: true */
 
 var gulp = require('gulp'),
+    remover = require('del'),
     HTMLValidator = require('gulp-html'),
     CSSCompiler = require('gulp-sass'),
     CSSValidator = require('gulp-w3c-css'),
@@ -8,6 +9,8 @@ var gulp = require('gulp'),
     config = require('./config.json'),
     color = config.colors,
     folders = config.folders,
+    contentLayer = config.content_layer,
+    settingsLayer = config.settings_layer,
     layer = null,
     pathToHTMLFile = null,
     pathToSassFile = null,
@@ -132,7 +135,7 @@ gulp.task('validateCSS', ['compileCSS'], function () {
 
     return gulp.src(pathToCSSFile)
         .pipe(new CSSValidator())
-        .pipe(gulp.dest(folders.validator.results.css));
+        .pipe(gulp.dest(folders.validator_results));
 });
 
 /**
@@ -202,4 +205,46 @@ gulp.task('compileCSS', function () {
             browsers: ['last 2 versions']
         }))
         .pipe(gulp.dest(pathToCSSFolder));
+});
+
+/**
+ * CLEAN
+ *
+ * This task deletes the removable files and folders created during development and
+ * production. A report is generated regarding files/folders that exist and can be
+ * removed, and those that do not exist.
+ */
+gulp.task('clean', function () {
+    'use strict';
+
+    var fileSystem = require('fs'),
+        index,
+        removableFolders = [
+            folders.validator_results,
+            folders.development +
+                folders.layers.content +
+                contentLayer.styles.target,
+            folders.development +
+                folders.layers.settings +
+                settingsLayer.styles.target
+        ];
+
+    for (index = 0; index < removableFolders.length; index += 1) {
+        try {
+            fileSystem.accessSync(removableFolders[index], fileSystem.F_OK);
+            process.stdout.write('\n\t' + color.green + removableFolders[index] +
+                color.default + ' was found and ' + color.green + 'will' +
+                color.default + ' be deleted.\n');
+            remover(removableFolders[index]);
+        } catch (error) {
+            if (error) {
+                process.stdout.write('\n\t' + color.red + removableFolders[index] +
+                    color.default + ' does ' + color.red + 'not' + color.default +
+                    ' exist or is ' + color.red + 'not' + color.default +
+                    ' accessible.\n');
+            }
+        }
+    }
+
+    process.stdout.write('\n');
 });
