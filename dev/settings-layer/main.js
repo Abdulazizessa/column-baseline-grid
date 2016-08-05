07565014a52f03044e32824fe0b6beeea3a62c22
Roy Vanegas <roy@thecodeeducators.com>
2016-08-04 22:15:36 -0400

    Format main.js so it complies with JSLint and ESLint

--- dev/settings-layer/main.js
+++ dev/settings-layer/main.js
@@ -1,17 +1,17 @@
+/*global chrome */
+
 window.onload = function() {
-   let powerSwitch = window.document.getElementById('power-switch');
+    let powerSwitch = window.document.getElementById('power-switch');
 
-   chrome.runtime.sendMessage('settingsOpened',function(response){
-      console.log(response.gridIsEnabled);
-      powerSwitch.checked = response.gridIsEnabled;
-   });
+    chrome.runtime.sendMessage('settingsOpened', function(response) {
+        powerSwitch.checked = response.gridIsEnabled;
+    });
 
-   powerSwitch.addEventListener('change',function(){
-      if(powerSwitch.checked){
-         chrome.runtime.sendMessage('enableGrid');
-      }
-      else{
-         chrome.runtime.sendMessage('disableGrid');
-      }
-   },false);
+    powerSwitch.addEventListener('change', function() {
+        if (powerSwitch.checked) {
+            chrome.runtime.sendMessage('enableGrid');
+        } else {
+            chrome.runtime.sendMessage('disableGrid');
+        }
+    }, false);
 };
